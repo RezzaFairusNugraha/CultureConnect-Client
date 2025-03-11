cebbb6d178757ae3f6eddfd8ac4f29940d857dcf
habibiahmada <habibiahmadaziz@gmail.com>
2025-03-12 03:49:07 +0700

    Fix server binding issue

--- src/api/index.js
+++ src/api/index.js
@@ -1,4 +1,4 @@
-const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || "http://localhost:5000";
+const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || "https://cultureconnect-server.up.railway.app";
 
 export const fetchDashboardData = async (token) => {
     try {
