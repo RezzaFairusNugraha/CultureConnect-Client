e903262de23db4216cbb6a6146bca20b384ba80b
Rezza Fairus Nugraha <rezafairusfugraha@gmail.com>
2025-03-13 23:06:32 +0700

    Update login,modal and register

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,9 +1,9 @@
 import { MdAlternateEmail } from "react-icons/md";
-import { FaFingerprint } from "react-icons/fa";
 import { FaRegEye, FaRegEyeSlash } from "react-icons/fa";
 import { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import { login } from "../api/index";
+import Layout from "../components/Layout/CommonLayout";
 
 const Login = () => {
   const [showPassword, setShowPassword] = useState(false);
@@ -16,7 +16,6 @@ const Login = () => {
   const handleLogin = async (e) => {
     e.preventDefault();
     setError("");
-
     try {
       const data = await login(email, password);
       localStorage.setItem("token", data.token);
@@ -28,59 +27,70 @@ const Login = () => {
   };
 
   return (
-    <div className="w-full h-screen flex items-center justify-center bg-sky-200">
-      <div className="w-[90%] max-w-sm md:max-w-md lg:max-w-md p-5 bg-[#e8e6fb] flex-col flex items-center gap-3 rounded-xl shadow-slate-500 shadow-lg">
-        <img src="/logo.png" alt="logo" className="w-12 md:w-14" />
-        <h1 className="text-lg md:text-xl font-semibold">Welcome Back</h1>
-        {error && <p className="text-red-500">{error}</p>}
-        <p className="text-xs md:text-sm text-gray-500 text-center">
-          Don't have an account?{" "}
-          <span className="text-sky-400">
-            <strong>
-              <Link to="/register">Sign up</Link>
-            </strong>
-          </span>
-        </p>
-        <form onSubmit={handleLogin} className="w-full flex flex-col gap-3">
-          <div className="w-full flex items-center gap-2 bg-white p-2 rounded-xl">
-            <MdAlternateEmail />
-            <input
-              type="email"
-              value={email}
-              onChange={(e) => setEmail(e.target.value)}
-              placeholder="Email address"
-              className="bg-transparent border-0 w-full outline-none text-sm md:text-base"
-            />
-          </div>
-
-          <div className="w-full flex items-center gap-2 bg-white p-2 rounded-xl relative">
-            <FaFingerprint />
-            <input
-              type={showPassword ? "password" : "text"}
-              value={password}
-              onChange={(e) => setPassword(e.target.value)}
-              placeholder="Password"
-              className="bg-transparent border-0 w-full outline-none text-sm md:text-base focus:bg-white"
-            />
-            {showPassword ? (
-              <FaRegEyeSlash
-                className="absolute right-5 cursor-pointer"
-                onClick={togglePasswordView}
+    <Layout>
+      <div className="min-h-screen flex items-center justify-center bg-gray-100 pb-30">
+        <div className="w-full max-w-md bg-white p-6 rounded-lg shadow-md">
+          <h3 className="text-2xl font-semibold text-gray-900 mb-6 text-center">
+            Masuk ke akun anda
+          </h3>
+          {error && <p className="text-red-500 text-sm mb-3">{error}</p>}
+          <form className="space-y-4" onSubmit={handleLogin}>
+            <div className="relative">
+              <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
+                Masukkan email
+              </label>
+              <input
+                type="email"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+                placeholder="name@company.com"
+                className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full  p-2.5"
+                required
               />
-            ) : (
-              <FaRegEye
-                className="absolute right-5 cursor-pointer"
-                onClick={togglePasswordView}
+            </div>
+            <div className="relative">
+              <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
+                Masukkan password
+              </label>
+              <input
+                type={showPassword ? "text" : "password"}
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+                placeholder="Password"
+                className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full pr-10 p-2.5"
+                required
               />
-            )}
-          </div>
-        </form>
-
-        <button className="w-full p-2 bg-transparent border border-black hover:border-[#74d4ff] rounded-xl mt-3 hover:bg-[#74d4ff]  text-sm md:text-base hover:text-white shadow hover:shadow-black">
-          Login
-        </button>
+              <span
+                className="absolute inset-y-0 right-0 flex items-center pr-3 cursor-pointer pt-6"
+                onClick={togglePasswordView}
+              >
+                {showPassword ? (
+                  <FaRegEyeSlash className="text-gray-400" />
+                ) : (
+                  <FaRegEye className="text-gray-400" />
+                )}
+              </span>
+            </div>
+            <button
+              type="submit"
+              className="w-full text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-semibold rounded-lg text-sm px-5 py-2.5 text-center"
+            >
+              Masuk
+            </button>
+            <div className="flex justify-between mt-2 text-sm">
+              <p className="text-xs md:text-sm text-gray-500 text-center">
+                Belum Punya Akun?{" "}
+                <span className="text-blue-700 border border-l-0 border-t-0 border-r-0 border-blue-700">
+                  <strong>
+                    <Link to="/register">Daftar Sekarang</Link>
+                  </strong>
+                </span>
+              </p>
+            </div>
+          </form>
+        </div>
       </div>
-    </div>
+    </Layout>
   );
 };
 
--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -3,15 +3,15 @@ import { FaFingerprint } from "react-icons/fa";
 import { VscAccount } from "react-icons/vsc";
 import { FaRegEye, FaRegEyeSlash } from "react-icons/fa";
 import { useState } from "react";
-import { Link, useNavigate } from "react-router-dom";
+import { Link } from "react-router-dom";
 import { register } from "../api/index";
+import Layout from "../components/Layout/CommonLayout";
 
 const Register = () => {
   const [form, setForm] = useState({ name: "", email: "", password: "" });
   const [error, setError] = useState("");
   const [showPassword, setShowPassword] = useState(false);
   const togglePasswordView = () => setShowPassword(!showPassword);
-  const navigate = useNavigate();
 
   const handleChange = (e) => {
     setForm({ ...form, [e.target.name]: e.target.value });
@@ -24,131 +24,92 @@ const Register = () => {
     try {
       await register(form.name, form.email, form.password);
       alert("Registrasi berhasil! Silakan login.");
-      navigate("/");
     } catch (err) {
       setError(err.message);
     }
   };
 
   return (
-    // <div className="flex justify-center items-center h-screen">
-    //   <form
-    //     onSubmit={handleSubmit}
-    //     className="p-6 bg-white rounded shadow-md w-80"
-    //   >
-    //     <h2 className="text-xl font-bold mb-4">Register</h2>
-    //     {error && <p className="text-red-500">{error}</p>}
-    //     <input
-    //       type="text"
-    //       name="name"
-    //       placeholder="Nama"
-    //       value={form.name}
-    //       onChange={handleChange}
-    //       className="border p-2 w-full mb-2"
-    //       required
-    //     />
-    //     <input
-    //       type="email"
-    //       name="email"
-    //       placeholder="Email"
-    //       value={form.email}
-    //       onChange={handleChange}
-    //       className="border p-2 w-full mb-2"
-    //       required
-    //     />
-    //     <input
-    //       type="password"
-    //       name="password"
-    //       placeholder="Password"
-    //       value={form.password}
-    //       onChange={handleChange}
-    //       className="border p-2 w-full mb-2"
-    //       required
-    //     />
-    //     <button
-    //       type="submit"
-    //       className="bg-blue-500 text-white p-2 w-full rounded"
-    //     >
-    //       Register
-    //     </button>
-    //     <p className="mt-4 text-center">
-    //       Sudah punya akun?{" "}
-    //       <Link to="/login" className="text-blue-500">
-    //         Login
-    //       </Link>
-    //     </p>
-    //   </form>
-    // </div>
-    <div className="w-full h-screen flex items-center justify-center bg-sky-200">
-      <div className="w-[90%] max-w-sm md:max-w-md lg:max-w-md p-5 bg-[#e8e6fb] flex-col flex items-center gap-3 rounded-xl shadow-slate-500 shadow-lg">
-        <img src="/logo.png" alt="logo" className="w-12 md:w-14" />
-        <h1 className="text-lg md:text-xl font-semibold">Welcome Back</h1>
-        {error && <p className="text-red-500">{error}</p>}
-
-        <form onSubmit={handleSubmit} className="w-full flex flex-col gap-3">
-          <div className="w-full flex items-center gap-2 bg-white p-2 rounded-xl">
-            <VscAccount />
-            <input
-              type="text"
-              name="name"
-              placeholder="Nama"
-              value={form.name}
-              onChange={handleChange}
-              required
-              className="bg-transparent border-0 w-full outline-none text-sm md:text-base"
-            />
-          </div>
-          <div className="w-full flex items-center gap-2 bg-white p-2 rounded-xl">
-            <MdAlternateEmail />
-            <input
-              type="email"
-              name="email"
-              placeholder="Email"
-              value={form.email}
-              onChange={handleChange}
-              required
-              className="bg-transparent border-0 w-full outline-none text-sm md:text-base"
-            />
-          </div>
-
-          <div className="w-full flex items-center gap-2 bg-white p-2 rounded-xl relative">
-            <FaFingerprint />
-            <input
-              type={showPassword ? "password" : "text"}
-              name="password"
-              placeholder="Password"
-              value={form.password}
-              onChange={handleChange}
-              required
-              className="bg-transparent border-0 w-full outline-none text-sm md:text-base focus:bg-white"
-            />
-            {showPassword ? (
-              <FaRegEyeSlash
-                className="absolute right-5 cursor-pointer"
-                onClick={togglePasswordView}
+    <Layout>
+      <div className="min-h-screen flex items-center justify-center bg-gray-100 pb-30">
+        <div className="w-full max-w-md bg-white p-6 rounded-lg shadow-md">
+          <h3 className="text-2xl font-semibold text-gray-900 mb-6 text-center">
+            Daftar Akun CultureConnect
+          </h3>
+          {error && <p className="text-red-500 text-sm mb-3">{error}</p>}
+          <form className="space-y-4" onSubmit={handleSubmit}>
+            <div className="relative">
+              <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
+                Nama Lengkap
+              </label>
+              <input
+                type="text"
+                name="name"
+                placeholder="Nama Lengkap"
+                value={form.name}
+                onChange={handleChange}
+                required
+                className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full  p-2.5"
               />
-            ) : (
-              <FaRegEye
-                className="absolute right-5 cursor-pointer"
-                onClick={togglePasswordView}
+            </div>
+            <div className="relative">
+              <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
+                Email
+              </label>
+              <input
+                type="email"
+                name="email"
+                placeholder="name@company.com"
+                value={form.email}
+                onChange={handleChange}
+                required
+                className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full  p-2.5"
               />
-            )}
-          </div>
-        </form>
-
-        <button className="w-full p-2 bg-transparent border border-black hover:border-[#74d4ff] rounded-xl mt-3 hover:bg-[#74d4ff]  text-sm md:text-base hover:text-white shadow hover:shadow-black ">
-          Daftar
-        </button>
-        <p className="text-xs md:text-sm text-gray-500 text-center">
-          Sudah punya akun?{" "}
-          <span className="text-sky-400">
-            <strong>
-              <Link to="/login">Login</Link>
-            </strong>
-          </span>
-        </p>
+            </div>
+            <div className="relative">
+              <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
+                Password
+              </label>
+              <input
+                type={showPassword ? "text" : "password"}
+                name="password"
+                placeholder="Masukan Password Baru"
+                value={form.password}
+                onChange={handleChange}
+                className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full pr-10 p-2.5"
+                required
+              />
+              <span
+                className="absolute inset-y-0 right-0 flex items-center pr-3 cursor-pointer pt-6"
+                onClick={togglePasswordView}
+              >
+                {showPassword ? (
+                  <FaRegEyeSlash className="text-gray-400" />
+                ) : (
+                  <FaRegEye className="text-gray-400" />
+                )}
+              </span>
+            </div>
+            <button
+              type="submit"
+              className="w-full text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-semibold rounded-lg text-sm px-5 py-2.5 text-center"
+            >
+              Masuk
+            </button>
+            <div className="flex justify-between mt-2 text-sm">
+              <p className="text-xs md:text-sm text-gray-500 text-center">
+                Sudah Punya Akun?{" "}
+                <span className="text-blue-700 border border-l-0 border-t-0 border-r-0 border-blue-700">
+                  <strong>
+                    <Link to="/login">Masuk Sekarang</Link>
+                  </strong>
+                </span>
+              </p>
+            </div>
+          </form>
+        </div>
       </div>
-    </div>
+    </Layout>
   );
 };
 
